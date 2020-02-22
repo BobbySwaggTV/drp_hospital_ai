df9824c3686b11d30555a38c672e151320b5aaf9
Vladyslav <53508222+DevVladyslav@users.noreply.github.com>
2020-02-21 20:11:09 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config						= {}
+
+Config.price			   = 200 --- Price you want to set when using the script
+Config.text                = "You have paid a medical bill of $" .. Config.price --- Dont touch the Config.price
+Config.enablepeds         = true    -- enable peds/disable peds 
+Config.pos = { -- postion of peds
+	{ x = 295.38,   y = -1447.22, z = 29.00, heading = 319.88 }
+}
+
